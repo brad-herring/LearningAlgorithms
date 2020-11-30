dea4e6df2e40a373c2d0345a2cd2741adb752959
Brad Herring <hbradherring@aol.com>
2020-11-30 11:01:27 -0500

    Classification with NFL Combine data 1

--- main.py
+++ main.py
@@ -4,49 +4,49 @@ import numpy as np
 import pandas as pd
 from IPython.display import clear_output
 
-
 import tensorflow as tf
 
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
-dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
-y_train = dftrain.pop('survived')
-y_eval = dfeval.pop('survived')
+CSV_COLUMN_NAMES = ['position', 'heightinchestotal', 'weight', 'fortyyd', 'twentyss', 'threecone', 'vertical', 'broad', 'bench']
+POSITION = ['RB', 'WR', 'OLB', 'FS', 'DE', 'TE', 'ILB', 'DT', 'P', 'QB', 'OG', 'OT', 'K', 'FB', 'SS', 'LS', 'CB', 'C', 'NT', 'OC']
 
-CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
-                       'embark_town', 'alone']
-NUMERIC_COLUMNS = ['age', 'fare']
+train = pd.read_csv(r'C:\Users\Admin\Desktop\Programming Applications and Projects\NFL Stuff\combine.csv')
+test = pd.read_csv(r'C:\Users\Admin\Desktop\Programming Applications and Projects\NFL Stuff\combine.csv')
 
-feature_columns = []
-for feature_name in CATEGORICAL_COLUMNS:
-  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
-  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+train_y = train.pop('position')
+test_y = test.pop('position')
 
-for feature_name in NUMERIC_COLUMNS:
-  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-print(feature_columns)
+def input_fn(features, labels, training=True, batch_size=256):
+  dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
-def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-  def input_function():  # inner function, this will be returned
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
-    if shuffle:
-      ds = ds.shuffle(1000)  # randomize order of data
-    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
-    return ds  # return a batch of the dataset
-  return input_function  # return a function object for use
+  if training:
+    dataset = dataset.shuffle(1000).repeat()
 
-train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
-eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+  return dataset.batch(batch_size)
 
-linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
 
-linear_est.train(train_input_fn)  # train
-result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key,))
+print(my_feature_columns)
 
-clear_output()  # clears consoke output
-print(result['accuracy'])
+# DNN with 2 hidden layers with 30 and 10 hidden nodes each.
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    hidden_units=[30, 10],
+    n_classes=3)
 
-pred_dicts = list(linear_est.predict(eval_input_fn))
-probs = pd.Series([pred['probabilities'][1] for pred in pred_dicts])
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+
+eval_result = classifier.evaluate(
+    input_fn=lambda: input_fn(test, test_y, training=False))
+
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+
+
+
 
 
