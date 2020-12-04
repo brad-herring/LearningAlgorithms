cf3ae0ebd8305d7b0deafa1678073fc9ff3bcb6e
Brad Herring <hbradherring@aol.com>
2020-12-04 11:56:06 -0500

    Working predictor feature

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ from IPython.display import clear_output
 
 import tensorflow as tf
 
-CSV_COLUMN_NAMES = ['position', 'heightinchestotal', 'weight', 'fortyyd', 'twentyss', 'threecone', 'vertical', 'broad', 'bench']
+CSV_COLUMN_NAMES = ['position', 'Height', 'Weight', 'FortyYard', 'TwentySS',
+                    'ThreeCone', 'Vertical', 'Broad', 'Bench']
+
 POSITIONS = {'RB': 1, 'WR': 2, 'OLB': 3, 'FS': 4, 'DE': 5, 'TE': 6, 'ILB': 7, 'DT': 8, 'P': 9, 'QB': 10, 'OG': 11,
              'OT': 12, 'K': 13, 'FB': 14, 'SS': 15, 'LS': 16, 'CB': 17, 'C': 18, 'NT': 19, 'OC': 20}
 
-train = pd.read_csv(r'C:\Users\Admin\Desktop\Programming Applications and Projects\NFL Stuff\combine.csv', header=0)
-test = pd.read_csv(r'C:\Users\Admin\Desktop\Programming Applications and Projects\NFL Stuff\combine.csv', header=0)
+train = pd.read_csv(r'C:\Users\Admin\Desktop\Programming Applications and Projects\NFL Stuff\combine.csv',
+                    names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(r'C:\Users\Admin\Desktop\Programming Applications and Projects\NFL Stuff\combine.csv',
+                   names=CSV_COLUMN_NAMES, header=0)
 
 train.position = [POSITIONS[item] for item in train.position]
 test.position = [POSITIONS[item] for item in test.position]
 
-train_y = train.pop('position')
-test_y = test.pop('position')
+train_target = train.pop('position')
+test_target = test.pop('position')
 
 
 def input_fn(features, labels, training=True, batch_size=256):
@@ -40,10 +44,40 @@ classifier = tf.estimator.DNNClassifier(
     n_classes=21)
 
 classifier.train(
-    input_fn=lambda: input_fn(train, train_y, training=True),
+    input_fn=lambda: input_fn(train, train_target, training=True),
     steps=10000)
 
 eval_result = classifier.evaluate(
-    input_fn=lambda: input_fn(test, test_y, training=False))
+    input_fn=lambda: input_fn(test, test_target, training=False))
 
 print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+
+def input_fn(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+
+features = ['Height', 'Weight', 'FortyYard', 'TwentySS',
+            'ThreeCone', 'Vertical', 'Broad', 'Bench']
+
+predict = {}
+
+print("Please type numeric values as prompted.")
+for feature in features:
+  valid = True
+  while valid:
+    val = input(feature + ": ")
+    if not val.isdigit(): valid = False
+    predict[feature] = [float(val)]
+    valid = False
+
+print(predict)
+
+predictions = classifier.predict(input_fn=lambda: input_fn(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        class_id, 100 * probability))
